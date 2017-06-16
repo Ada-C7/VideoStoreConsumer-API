120f8ad8aec484c1029ee0ae63c69690f2b146cf
Haby Randall <sow.haby@gmail.com>
2017-06-16 10:49:53 -0700

    Working on create movie method in rails api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,15 +13,35 @@ class MoviesController < ApplicationController
 
   def show
     render(
-      status: :ok,
-      json: @movie.as_json(
-        only: [:title, :overview, :release_date, :inventory],
-        methods: [:available_inventory]
-        )
-      )
+    status: :ok,
+    json: @movie.as_json(
+    only: [:title, :overview, :release_date, :inventory],
+    methods: [:available_inventory]
+    )
+    )
+  end
+
+  def create
+    existing_movie = Movie.find_by(title: movie_params[:title])
+
+    if existing_movie
+      render json: {errors: {movie: ["#{params[:title]} is already in Video Store"]}}
+    else
+      movie = Movie.new(movie_params)
+      if movie.save
+        render json: movie.as_json, status: :ok
+      else
+        render json: {errors: {movie: ["An error occurred"]}}
+      end 
+    end
+
   end
 
   private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory, :image_url)
+  end
 
   def require_movie
     @movie = Movie.find_by(title: params[:title])
