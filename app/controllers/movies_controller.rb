7c1fa736c00dd5826525dd78544bd18e1f3e9c56
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-19 11:36:48 -0700

    can create new movie from API

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,16 @@ class MoviesController < ApplicationController
       )
   end
 
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: {id: movie.id}, status: :ok
+    else
+      render json: { errors: movie.errors.messages }, status: :bad_request
+    end
+  end
+
   private
 
   def require_movie
@@ -28,5 +38,9 @@ class MoviesController < ApplicationController
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory, :image_url)
   end
 end
