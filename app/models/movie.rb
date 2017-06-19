9fcbf263ad0b979eb09fcd694d78c7a5a13f7b9e
Theresa Manney <theresa.m.17@gmail.com>
2017-06-19 15:31:40 -0700

    Corrected picture url

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,10 +10,16 @@ class Movie < ApplicationRecord
 
   def image_url
     orig_value = read_attribute :image_url
+    puts "original value"
+    puts orig_value
     if !orig_value
       MovieWrapper::DEFAULT_IMG_URL
+    elsif orig_value.include? MovieWrapper::DEFAULT_IMG_SIZE
+      orig_value
     else
       MovieWrapper.construct_image_url(orig_value)
+      # puts "movie image"
+      # puts movie_image
     end
   end
 end
