6d80d75348e783e2ed3dbd7c65f63626c2561c22
Marisol Lopez <lmarisolopez@gmail.com>
2017-06-21 14:08:49 -0700

    CRUD and routes for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -13,12 +13,44 @@ class CustomersController < ApplicationController
     data = data.paginate(page: params[:p], per_page: params[:n])
 
     render json: data.as_json(
-      only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit],
-      methods: [:movies_checked_out_count]
+    only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit],
+    methods: [:movies_checked_out_count]
     )
   end
 
-private
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.create(customer_params)
+  end
+
+  def update
+    @customer = Customer.find_by(id: params[:id])
+    if @customer.nil?
+      head :not_found
+    else
+      @customer.update_attributes(customer_params)
+      redirect_to customer_path
+    end
+  end
+
+  def edit
+    @customer = Customer.find_by(id: params[:id])
+  end
+
+  def destroy
+    @customer = Customer.find_by(id: params[:id])
+    @customer.destroy
+    redirect_to customers_path
+  end
+
+  private
   def parse_query_args
     errors = {}
     @sort = params[:sort]
@@ -30,4 +62,9 @@ private
       render status: :bad_request, json: { errors: errors }
     end
   end
+
+  def customer_params
+    return params.require(:customer).permit(:name, :address, :city, :state, :postal_code, :phone, :account_credit)
+  end
+
 end
